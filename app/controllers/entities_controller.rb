57ac80bf042645ef0bd3df76c154459b885542bd
misterjunio <ruca.33.maia@gmail.com>
2016-03-02 17:57:20 +0000

    typeSquare in Ruby, can create entity types locally

---
+++ app/controllers/entities_controller.rb
@@ -0,0 +1,44 @@
+class EntitiesController < ApplicationController
+  def new
+    temp = EntityType.new("#{params[:entity][:entity_name]}", {});
+    @entity = temp.type_name
+  end
+end
+
+require 'date'
+
+class EntityType
+  attr_accessor :type_name
+  attr_accessor :property_names_and_types
+  def initialize(type_name, property_names_and_types)
+    @type_name = type_name
+    @property_names_and_types = property_names_and_types
+  end
+  def property_type(property_name)
+    @property_names_and_types[property_name]
+  end
+end
+
+class Entity
+  attr_accessor :entity_type
+  attr_accessor :properties
+  def initialize(entity_type, attrs = {})
+    @entity_type = entity_type
+    bind_properties(entity_type.property_names_and_types)
+    attrs.each do |name, value|
+      instance_variable_set("@#{name}", value)
+    end
+  end
+  def bind_properties(property_names_and_types)
+    (class << self; self; end).module_eval do
+      property_names_and_types.each do |name, type|
+        define_method name.to_sym do
+          instance_variable_get("@#{name}")
+        end
+        define_method "#{name}=" do |value|
+          instance_variable_set("@#{name}", value)
+        end
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  root                        'static_pages#home'
+  
+  post    'add_entity' =>      'entities#new'
+  get     'remove_class' =>   'static_pages#home'
+  delete  'remove_class' =>   'entities#delete'
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
